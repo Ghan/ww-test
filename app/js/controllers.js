f1e03d29f7959ec48ab56d8b5cfd1317a1ed342f
Ghan <ghanpatel@gmail.com>
2014-06-24 17:38:55 -0400

    adds filter by id

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,13 +4,29 @@
 
 angular.module('myApp.controllers', [])
   .controller('mapPageCtrl', ['$scope', 'markerListService', function($scope, markerListService) {
+      $scope.ids = [];
+      $scope.filteredID = 0;
+      $scope.data;
+      $scope.markers = [];
+      
       markerListService.fetch().then(function(data) {
           $scope.data = data;
           for (var i = 0; i < data.length; i++){
               createMarker(data[i]);
+              if($.inArray(data[i].positionable_id, $scope.ids) === -1){
+                $scope.ids.push(data[i].positionable_id);
+              }
               // {"positionable_id":272,"latitude":"40.7633078524","longitude":"-73.9674413711","speed":null,"heading":"-1.0","created_at":"2014-04-22T20:00:03.000-04:00"}
           }
-      }); 
+      });
+      
+      $scope.$watch('filteredID', function() {
+        if($scope.filteredID > 0){
+          resetMap($scope.filteredID);
+        } else {
+          resetMap("all");
+        }
+      });
       
       var mapOptions = {
         zoom: 12,
@@ -19,8 +35,6 @@ angular.module('myApp.controllers', [])
       };
 
       $scope.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-      $scope.markers = [];
 
       var infoWindow = new google.maps.InfoWindow();
 
@@ -39,6 +53,15 @@ angular.module('myApp.controllers', [])
         });
 
         $scope.markers.push(marker);
+      }
+      
+      var resetMap = function(id){
+        for(var i = 0; i < $scope.markers.length; i++){
+          $scope.markers[i].setMap(null);
+          if($scope.markers[i].title === id || id === "all"){
+            $scope.markers[i].setMap($scope.map);
+          }
+        }
       }
 
       $scope.openInfoWindow = function(e, selectedMarker){
